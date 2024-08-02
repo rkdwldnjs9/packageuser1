54dc77e9b736d49947433c514b7a525f181061ef
jwkang96 <jiwon961212@hanmail.net>
2024-08-02 16:43:35 +0900

    Initial commit

---
+++ packageuser1/test_addition.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+from packageuser1.addition import add
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
---
+++ packageuser1/test_subtraction.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+from packageuser1.subtraction import subtract
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(-1, -1) == 0
+    assert subtract(0, 1) == -1
